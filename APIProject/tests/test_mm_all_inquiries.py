9fe69743735458ba19f22618484efe5275ee198e
sangavai <sangupari@gmail.com>
2026-01-10 09:32:58 -0500

    changed base_url name

--- APIProject/tests/test_mm_all_inquiries.py
+++ APIProject/tests/test_mm_all_inquiries.py
@@ -1,12 +1,12 @@
 import logging
 import requests
-from config import headers,base_url
+from config import headers,base_url_key
 from api_utils.validator import validate_json_key
 
 
 def test_mm():
 
-    url = base_url + "/objects"
+    url = base_url_key + "/objects"
     response = requests.get(url=url,headers=headers)
     assert response.status_code == 200, f"Not expected status code"
     logging.info("TEST PASSED")
