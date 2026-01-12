291df4dea1bdd4fbe4358d544e1a0289044f86f8
sangavai <sangupari@gmail.com>
2026-01-11 19:36:30 -0500

    changed hard coding

--- APIProject/tests/test_api.py
+++ APIProject/tests/test_api.py
@@ -3,14 +3,14 @@ import json
 
 import os
 import logging
-from APIProject.config import x_api_key
+from APIProject.config import x_api_key, base_url_key
 def test_api():
     
     #api_key = os.getenv("x-api-key")
     header = {
         "x-api-key" : x_api_key
     }
-    response = requests.get("https://reqres.in/api/users",headers=header)
+    response = requests.get(base_url_key,headers=header)
     try:
         assert response.status_code == 200
         logging.info("API KEY AUTHENTICATION TESTED")
